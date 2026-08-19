1d679b76433c1de1dcc19428b1c6c0f9cfdd5e58
yanzinhomt18-max <yanzinho.mt18@gmail.com>
2026-08-19 00:10:22 -0300

    V9: ativa Projeto/Obra e Service Worker

--- v9/src/main.tsx
+++ v9/src/main.tsx
@@ -1,9 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { ProjectProvider } from './project/ProjectContext'
+
+if ('serviceWorker' in navigator && import.meta.env.PROD) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').catch(() => undefined)
+  })
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ProjectProvider>
+      <App />
+    </ProjectProvider>
   </StrictMode>
 )
