6d6e538a5c1597ab3bc22f31b3f57c6bcb1bcedc
yanzinhomt18-max <yanzinho.mt18@gmail.com>
2026-08-19 00:10:06 -0300

    V9: adiciona Service Worker seguro

---
+++ v9/public/sw.js
@@ -0,0 +1,42 @@
+const CACHE = 'impertudo-v9-alpha3'
+const CORE = ['/', '/manifest.webmanifest', '/icon.svg']
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(CORE)))
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request
+  if (request.method !== 'GET') return
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request)
+        .then((response) => {
+          const copy = response.clone()
+          caches.open(CACHE).then((cache) => cache.put('/', copy))
+          return response
+        })
+        .catch(() => caches.match('/'))
+    )
+    return
+  }
+
+  event.respondWith(
+    caches.match(request).then((cached) => cached || fetch(request).then((response) => {
+      if (response.ok && new URL(request.url).origin === self.location.origin) {
+        const copy = response.clone()
+        caches.open(CACHE).then((cache) => cache.put(request, copy))
+      }
+      return response
+    }))
+  )
+})
